8dff5fe3508a396a15898726b228c04e888a3619
Andrei Chenchik <andrei@chenchik.me>
2022-07-14 10:49:05 +0200

    2.3.2

---
+++ Тема 2. Аналитические СУБД. Vertica/3. Знакомство с Vertica/Задание 2/realization.sql
@@ -0,0 +1,7 @@
+select 
+    'HELLO, WORLD!' as a1, 
+    2 + 5 as a2, 
+    TO_DATE('2022-05-09','YYYY-MM-DD') as a3, 
+    CAST('5 days' AS INTERVAL) as a4;
+
+-- Двигайтесь дальше! Ваш код: CSzq1XRDCF
