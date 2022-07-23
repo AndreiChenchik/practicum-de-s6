49f261e7ddcb9e96585039f49301f83d55d60798
Andrei Chenchik <andrei@chenchik.me>
2022-07-23 20:06:08 +0200

    2.9.1

---
+++ Тема 2. Аналитические СУБД. Vertica/9. Выбираем параметры сегментации/Задание 1/realization.sql
@@ -0,0 +1,12 @@
+create table dialogs
+(
+    message_id   int PRIMARY KEY,
+    message_ts   timestamp(6),
+    message_from int REFERENCES users(id),
+    message_to int REFERENCES users(id),
+    message varchar(1000),
+    message_type varchar(100)
+)
+SEGMENTED BY hash(message_id) all nodes;
+
+-- Двигайтесь дальше! Ваш код: eMRDEiAmcc
