68e6a7e42442b61b01b72c7ae859539fcfff7072
Andrei Chenchik <andrei@chenchik.me>
2022-07-24 08:28:36 +0200

    2.11.1

--- Тема 2. Аналитические СУБД. Vertica/11. Удаление и обновление данных в Vertica/Задание 1/realization.sql
+++ Тема 2. Аналитические СУБД. Vertica/11. Удаление и обновление данных в Vertica/Задание 1/realization.sql
@@ -1,11 +1,13 @@
 SET SESSION AUTOCOMMIT TO off;
 
-DELETE FROM xxx WHERE xxx > 123;
+DELETE FROM members WHERE age > 45;
 
-SELECT ххх, ххх, ххх FROM DELETE_VECTORS
-	where ххх like 'users%';
+SELECT node_name, projection_name, deleted_row_count FROM DELETE_VECTORS
+	where projection_name like 'members%';
 
-SELECT func(ххх) FROM DELETE_VECTORS
-	where xxx like 'users%';
+SELECT max(deleted_row_count) FROM DELETE_VECTORS
+	where projection_name like 'members%';
 
 ROLLBACK;
+
+-- Двигайтесь дальше! Ваш код: X74a7fpwqI
--- Тема 2. Аналитические СУБД. Vertica/11. Удаление и обновление данных в Vertica/Задание 1/submit.py
+++ Тема 2. Аналитические СУБД. Vertica/11. Удаление и обновление данных в Vertica/Задание 1/submit.py
@@ -1,37 +1,44 @@
 import os
- 
+
 import requests
 
-def submit(t_code, rlz_file='', conn=''):
-    user_code = ''
+
+def submit(t_code, rlz_file="", conn=""):
+    user_code = ""
     if rlz_file:
         full_lesson_path = os.path.dirname(os.path.abspath(__file__))
-        user_file = f'{full_lesson_path}/{rlz_file}'
+        user_file = f"{full_lesson_path}/{rlz_file}"
 
-        with open(user_file, 'r') as u_file:
+        with open(user_file, "r") as u_file:
             user_code = u_file.read()
 
     if conn:
         full_lesson_path = os.path.dirname(os.path.abspath(__file__))
-        user_file = f'{full_lesson_path}/conn.py'
+        user_file = f"{full_lesson_path}/conn.py"
 
-        with open(user_file, 'r') as u_file:
+        with open(user_file, "r") as u_file:
             user_conn = u_file.read()
+            user_conn = user_conn.replace(
+                "VERTICA_HOST", os.environ["VERTICA_HOST"]
+            )
+            user_conn = user_conn.replace(
+                "VERTICA_PORT", os.environ["VERTICA_PORT"]
+            )
+            user_conn = user_conn.replace(
+                "VERTICA_USER", os.environ["VERTICA_USER"]
+            )
+            user_conn = user_conn.replace(
+                "VERTICA_PASSWORD", os.environ["VERTICA_PASSWORD"]
+            )
 
     r = requests.post(
-        'http://localhost:3002',
-        json={
-            "code": user_code,
-            "test": t_code,
-            "conn": user_conn
-            })
-
-    print(r.json()['stderr'].replace('__test',rlz_file[:-3]))
-    print(r.json()['stdout'].replace('__test',rlz_file[:-3]))
-
-if __name__ == '__main__':
-    submit(
-        'de06021101',
-        'realization.sql',
-        'conn.py'
+        "http://localhost:3002",
+        json={"code": user_code, "test": t_code, "conn": user_conn},
     )
+
+    print(r.json()["stderr"].replace("__test", rlz_file[:-3]))
+    print(r.json()["stdout"].replace("__test", rlz_file[:-3]))
+
+
+if __name__ == "__main__":
+    submit("de06021101", "realization.sql", "conn.py")
